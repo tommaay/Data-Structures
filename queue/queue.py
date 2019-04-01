35821a45f327ba1d4e34ebda35b0555e64a1d1b1
ToMMaaY <thuynh629@gmail.com>
2019-04-01 12:33:20 -0700

    completed queue.py and passed all of the tests

--- queue/queue.py
+++ queue/queue.py
@@ -1,18 +1,93 @@
-from linked_list import LinkedList
+class Node:
+    def __init__(self, value=None, next_node=None):
+        # its own value
+        self.value = value
+        # the next node in the list
+        self.next_node = next_node
+
+    def get_value(self):
+        # return the value of this node
+        return self.value
+
+    def get_next(self):
+            # return a reference to this node's next_node property
+        return self.next_node
+
+    def set_next(self, new_next):
+        # set this node's `next_node` property
+        self.next_node = new_next
 
 
 class Queue:
     def __init__(self):
         self.size = 0
-        # what data structure should we
-        # use to store queue elements?
-        self.storage = LinkedList()
+        # reference the first node in the linked list
+        self.head = None
+        # reference to the last node in the linked list
+        self.tail = None
 
-    def enqueue(self, item):
-        pass
+    # add a new node to the tail of our list
 
+    def enqueue(self, value):
+        new_node = Node(value)
+        # we have an empty linked list
+        # check the head reference
+        if self.head is None and self.tail is None:
+            # both self.head and self.tail to point
+            # to the new node we're adding
+            self.head = new_node
+            self.tail = new_node
+        # every other case
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+
+    # remove and return the value of the head node
     def dequeue(self):
-        pass
+        # make sure our list isn't empty
+        if self.head is None and self.tail is None:
+            return None
+        # take another reference to the head we're
+        # about to delete
+        old_head_value = self.head.value
+        # we only have a single node in the list
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+            return old_head_value
+        # we have at least two nodes in the list
+        else:
+            # move the self.head reference to refer
+            # to the old head's next node
+            self.head = self.head.get_next()
+            return old_head_value
+
+    def contains(self, target):
+        # make sure we don't have an empty list
+        if not self.head and not self.tail:
+            return False
+        # init a current reference to refer to the current
+        # node we're iterating on
+        current = self.head
+        # while the current node in the list is a valid Node
+        while current:
+            # check if the current node's value matches
+            # what we're looking for
+            if current.value == target:
+                return True
+            # update the current reference to the next
+            # node in the list
+            current = current.get_next()
+        # we've traversed the entire list
+        return False
 
     def len(self):
-        pass
+        length = 0
+        current_value = self.head
+
+        while current_value != None:
+            length += 1
+            current_value = current_value.get_next()
+        self.size = length
+
+        return self.size
